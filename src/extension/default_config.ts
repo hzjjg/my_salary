19646d4270b3383af7da2600992b577176e95f98
hzj <hzjio@outlook.com>
2021-07-23 17:59:47 +0800

    delete: delete default_config.ts

--- src/extension/default_config.ts
+++
@@ -1,9 +0,0 @@
-const options: Options = {
-    updateInterval: 100,
-    dailySalary: 600,
-    unit: '元',
-    startWorkingTime: '9:30',
-    endWorkingTime: '18:30',
-};
-
-export default options;
